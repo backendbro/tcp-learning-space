7092fa706b2a87c690db5fc5c0754735882b2e12
backendbro <ukagha.nzubechukwu@yahoo.com>
2023-10-10 08:21:03 -0400

    Added more description

--- tcp-raw.js
+++ tcp-raw.js
@@ -1,7 +1,7 @@
 const net = require('net')
 
 const server = net.createServer (socket => {
-    socket.write("hello")
+    socket.write("Welcome, state your name and tech stack \n")
     socket.on("data", (data) => {
         console.log(data.toString())
     })
